664891a9b6acf40f17c6746b36f9e29a554e9038
llilay <2316672811@qq.com>
2018-10-16 05:39:53 +0000

    微信支付

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,5 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         // 由于我们这个 URL 是给支付宝服务器调用的，肯定不会有 CSRF Token
         'payment/alipay/notify',
+        'payment/wechat/notify',
     ];
 }
