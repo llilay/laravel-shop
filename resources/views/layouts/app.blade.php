bcc73d09d132cf6328fec6f0ef5d304c581a8d2f
llilay <2316672811@qq.com>
2018-11-21 02:45:51 +0000

    管理后台 - 新增\修改\删除优惠券

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,24 @@
     <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- GrowingIO Analytics code version 2.1 -->
+    <!-- Copyright 2015-2018 GrowingIO, Inc. More info available at http://www.growingio.com -->
+
+    <script type='text/javascript'>
+        !function(e,t,n,g,i){e[i]=e[i]||function(){(e[i].q=e[i].q||[]).push(arguments)},n=t.createElement("script"),tag=t.getElementsByTagName("script")[0],n.async=1,n.src=('https:'==document.location.protocol?'https://':'http://')+g,tag.parentNode.insertBefore(n,tag)}(window,document,"script","assets.growingio.com/2.1/gio.js","gio");
+        gio('init','b4e838ccff38762d', {});
+
+        //custom page code begin here
+
+        //custom page code end here
+
+        gio('send');
+
+    </script>
+
+    <!-- End GrowingIO Analytics code version: 2.1 -->
+
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
