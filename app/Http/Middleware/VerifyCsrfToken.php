1ea183e55e7f799a73f4c07a8f186f10ac45a1d0
tanying <2316672811@qq.com>
2018-11-29 13:08:50 +0800

    支付宝分期还款

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -16,5 +16,6 @@ class VerifyCsrfToken extends Middleware
         'payment/alipay/notify',
         'payment/wechat/notify',
         'payment/wechat/refund_notify',
+        'installments/alipay/notify',
     ];
 }
